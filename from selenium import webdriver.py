e82e4730191aef95effc5e855342473fb5eac0ba
jenjen <jenjenahmad47@gmail.com>
2022-12-14 10:48:19 +0700

    Here is my first commit

---
+++ from selenium import webdriver.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time  
+
+print("Test case started")
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized");
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://sanbercode.com/")
+time.sleep(20)
+driver.close()
+print("\nTest case successfully completed")
